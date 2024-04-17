ce056292b58ae5693ace62646458148e034735eb
quan0715 <quan787887@gmail.com>
2024-04-18 03:16:02 +0800

    remove key

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -79,7 +79,39 @@ describe('home page spec', () => {
     cy.get('#blog-list-footer').should('exist')
     cy.scrollTo('bottom')
     cy.wait(3000)
+    //
+    // cy.get('#blog-list-footer').scrollIntoView()
+    // cy.wait(1000)
     cy.get('#blog-list-footer').should('have.text', 'No more issues')
   })
 
+  // it('test home page with user', () => {
+  //   cy.login()
+  // })
+
+  // it('get oauth button', () => {
+  //   // cy.login()
+  //   cy.visit('http://localhost:3000')
+  //   // get oauth button
+  //   const button = cy.get('#oauth-button')
+  //   button.should('exist')
+  //
+  //   // submit gitHub login form
+  //   // button.click()
+  //
+  //   // cy.origin('https://github.com', () => {
+  //   //   cy.get('#login_field').type('quan0715')
+  //   //   cy.get('#password').type('')
+  //   //   cy.get('input[name="commit"]').click()
+  //   //
+  //   //   cy.log('submit login form')
+  //   //
+  //   // })
+  //
+  //
+  //   console.log('token', token)
+  //
+  //   // check user avatar exist
+  //   cy.get('#user-avatar').should('exist')
+  // })
 })
