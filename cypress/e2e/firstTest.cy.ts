90dbef12d1ed5bd16063b437348f7541232568a1
quan0715 <quan787887@gmail.com>
2024-04-18 02:00:37 +0800

    test

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -42,10 +42,29 @@ Cypress.Commands.add('login', () => {
 })
 
 describe('home page spec', () => {
-
-
-
   it('get oauth button', () => {
-    cy.login()
+    // cy.login()
+    cy.visit('http://localhost:3000')
+    // get oauth button
+    const button = cy.get('#oauth-button')
+    button.should('exist')
+
+    // submit gitHub login form
+    button.click()
+
+    cy.origin('https://github.com', () => {
+      cy.get('#login_field').type('quan0715')
+      cy.get('#password').type('H125920690quan')
+      cy.get('input[name="commit"]').click()
+
+      cy.log('submit login form')
+
+    })
+
+    const token = cy.getCookie('access_token').should('exist')
+    console.log('token', token)
+
+    // check user avatar exist
+    cy.get('#user-avatar').should('exist')
   })
 })
