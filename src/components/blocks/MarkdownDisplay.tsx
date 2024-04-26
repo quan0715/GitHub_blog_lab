1ebc328bbb35d17f24233e7e3e3683972c132188
quan0715 <quan787887@gmail.com>
2024-04-26 13:45:12 +0800

    add scroll motion

--- src/components/blocks/MarkdownDisplay.tsx
+++ src/components/blocks/MarkdownDisplay.tsx
@@ -3,14 +3,22 @@ import {serialize} from "next-mdx-remote/serialize";
 import React from "react";
 import {MDXRemote} from "next-mdx-remote/rsc";
 import Markdown from 'react-markdown'
-
+import { motion, useScroll, useMotionValueEvent } from "framer-motion"
 export function MarkdownDisplay({source}: {source: string}) {
 
 
+
+    const { scrollYProgress } = useScroll()
+
+    // useMotionValueEvent(scrollY, "change", (latest) => {
+    //     console.log("Page scroll: ", latest)
+    // })
     return (
         <article className="max-w-none w-full prose prose-base prose-slate dark:prose-invert">
-            {/*<motion.div style={{*/}
-            {/*    scaleX:*/}
+            <motion.div className={"fixed top-0 left-0 right-0 transition-transform bg-primary h-2 rounded-2xl"} style={{
+                transformOrigin: 0,
+                scaleX: scrollYProgress,
+            }} />
             <Markdown>{source}</Markdown>
             {/*}}/>*/}
         </article>
