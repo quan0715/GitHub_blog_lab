8da8b81c1c8dd5e9f1d8bf866f586968ac9a118d
quan0715 <quan787887@gmail.com>
2024-04-26 13:38:51 +0800

    use frame motion

--- src/components/blocks/MarkdownDisplay.tsx
+++ src/components/blocks/MarkdownDisplay.tsx
@@ -1,12 +1,18 @@
+'use client'
 import {serialize} from "next-mdx-remote/serialize";
 import React from "react";
 import {MDXRemote} from "next-mdx-remote/rsc";
 import Markdown from 'react-markdown'
 
 export function MarkdownDisplay({source}: {source: string}) {
+
+
     return (
         <article className="max-w-none w-full prose prose-base prose-slate dark:prose-invert">
+            {/*<motion.div style={{*/}
+            {/*    scaleX:*/}
             <Markdown>{source}</Markdown>
+            {/*}}/>*/}
         </article>
     );
 }
--- src/components/blocks/client/IssueDisplayList.tsx
+++ src/components/blocks/client/IssueDisplayList.tsx
@@ -1,20 +1,21 @@
 'use client'
-import { useInfiniteQuery } from '@tanstack/react-query'
-import {getAllIssue, getIssueResultProps} from "@/actions/githubIssue";
-import {IssueCoverCard} from "@/components/blocks/IssueCoverCard";
-import {issueDataModelProps} from "@/models/IssueModel";
-import {useEffect , useState, useTransition} from "react";
-import {useInView} from "react-intersection-observer";
-import {Loader2} from "lucide-react";
+import { getAllIssue } from "@/actions/githubIssue"
+import { IssueCoverCard } from "@/components/blocks/IssueCoverCard"
+import { issueDataModelProps } from "@/models/IssueModel"
+import { useEffect, useRef, useState, useTransition } from "react"
+import { Loader2 } from "lucide-react"
+import { useInView } from "framer-motion"
 export function IssueDisplayList({ issueData, nextURL } : { issueData: issueDataModelProps[], nextURL?: string}) {
 
     const [_issueData, setIssueData] = useState<issueDataModelProps[]>(issueData)
     const [_nextURL, setNextURL] = useState<string | undefined>(nextURL)
-    const [ ref, inView,  ] = useInView()
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+    // const [ ref, inView,  ] = useInView()
     const [isPending, startTransition] = useTransition();
     console.log(nextURL)
     useEffect(() => {
-        if(inView && _nextURL !== undefined){
+        if(isInView && _nextURL !== undefined){
             startTransition(
                 async  () => {
                     const res = await getAllIssue(_nextURL)
@@ -25,20 +26,18 @@ export function IssueDisplayList({ issueData, nextURL } : { issueData: issueData
                 }
             )
         }
-    }, [inView, _nextURL, _issueData])
+    }, [isInView, _nextURL, _issueData])
 
     return (
         <div className={"w-full"}>
             <div className={"grid grid-cols-1 md:grid-cols-2 gap-4 py-4"}>
                 {
-                    // infiniteQuery.data?.pages.map((issue) => {
                     issueData!==undefined
                     ? _issueData.map((item) => {
                         return (
                             <IssueCoverCard key={item.number} issue={item}/>
                         )
                     }) : null
-                    // })
                 }
             </div>
             <div id={"blog-list-footer"} ref={ref} className={"w-full flex justify-center items-center"}>
@@ -48,8 +47,6 @@ export function IssueDisplayList({ issueData, nextURL } : { issueData: issueData
                         :  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                 }
             </div>
-
-
         </div>
     );
 }
